673801690af0e6feedb08a792514f4c83183247e
mrwindupbirdesq <nmaahs11@gmail.com>
2024-05-01 15:27:15 -0700

    Add more place data to info window

--- src/components/markerWithInfoWindow/MarkerWithInfoWindow.jsx
+++ src/components/markerWithInfoWindow/MarkerWithInfoWindow.jsx
@@ -9,7 +9,6 @@ import styles from './marker-with-info-window.module.css';
 function MarkerWithInfowindow({ index, placeData }) {
   const [infowindowOpen, setInfowindowOpen] = useState(false);
   const [markerRef, marker] = useAdvancedMarkerRef();
-
   function getCurrentDayHours() {
     const date = new Date();
     let dayIndex = date.getDay(); // JavaScript's getDay() returns 0 for Sunday, 1 for Monday, ..., 6 for Saturday
@@ -34,13 +33,26 @@ function MarkerWithInfowindow({ index, placeData }) {
       {infowindowOpen && (
         <InfoWindow
           anchor={marker}
-          maxWidth={200}
+          maxWidth={400}
           onCloseClick={() => setInfowindowOpen(false)}
         >
           <div className={styles.infoCard}>
-            <h4> {placeData.displayName.text}</h4>
-            <p>{placeData.rating}/5</p>
+            <h4>
+              {' '}
+              <a href={placeData.googleMapsUri}>{placeData.displayName.text}</a>
+            </h4>
+            <p>{placeData.shortFormattedAddress}</p>
+            <p>{placeData.nationalPhoneNumber}</p>
+            <p>
+              Rating: {placeData.rating}/5{' '}
+              <span style={{ color: 'grey' }}>
+                ({placeData.userRatingCount} reviews)
+              </span>
+            </p>
             <p>{getCurrentDayHours()}</p>
+            {placeData.websiteUri && (
+              <a href={placeData.websiteUri}>{placeData.websiteUri}</a>
+            )}
           </div>
         </InfoWindow>
       )}
