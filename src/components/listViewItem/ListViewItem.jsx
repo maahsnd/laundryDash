06b462d7c37bb53c55bfd9009df00fc07684457c
mrwindupbirdesq <nmaahs11@gmail.com>
2024-05-17 18:08:16 -0700

    Implement basic item

--- src/components/listViewItem/ListViewItem.jsx
+++ src/components/listViewItem/ListViewItem.jsx
@@ -1,7 +1,22 @@
 import styles from './list-view-item.module.css';
 
 function ListViewItem({ itemData }) {
-  return <div className={styles.itemContainer}></div>;
+  return (
+    <div className={styles.itemContainer}>
+      <h4>
+        <a href={itemData.googleMapsUri}>{itemData.displayName.text}</a>
+      </h4>
+      <p>{itemData.shortFormattedAddress}</p>
+      <p>{itemData.nationalPhoneNumber}</p>
+      <p>
+        Rating: {itemData.rating}/5{' '}
+        <span style={{ color: 'grey' }}>
+          ({itemData.userRatingCount} reviews)
+        </span>
+      </p>
+      <hr />
+    </div>
+  );
 }
 
 export default ListViewItem;
