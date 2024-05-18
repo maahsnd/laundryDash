f95c3cdb7a0c2cb658cfaa41a4418836ff020f37
mrwindupbirdesq <nmaahs11@gmail.com>
2024-05-18 15:23:44 -0700

    Integrate sort, clean up style

--- src/components/listView/ListView.jsx
+++ src/components/listView/ListView.jsx
@@ -1,19 +1,37 @@
 import styles from './list-view.module.css';
 import ListViewItem from '../listViewItem/ListViewItem';
+import { useState, useEffect } from 'react';
 
 function ListView({ laundryServices, sponsoredServices }) {
-  const sortOptions = (places) => {
-    byRating: () => [...places].sort((a, b) => b.rating - a.rating);
+  const [sortedServices, setSortedServices] = useState([]);
+  const [sortOption, setSortOption] = useState('byRating');
+
+  const sortOptions = {
+    byRating: () => [...laundryServices].sort((a, b) => b.rating - a.rating)
   };
+
+  useEffect(() => {
+    const sorted = sortOptions[sortOption]();
+    const services = [...sponsoredServices, ...sorted];
+    setSortedServices(services);
+  }, [sortOption, laundryServices, sponsoredServices]);
 
   return (
     <div className={styles.listContainer}>
-      {sponsoredServices.length > 0 &&
-        sponsoredServices.map((service) => (
-          <ListViewItem itemData={service} className={styles.sponsored} />
+      <div className={styles.listDash}>
+        <select
+          onChange={(e) => setSortOption(e.target.value)}
+          value={sortOption}
+        >
+          <option value="byRating">Sort by Rating</option>
+        </select>
+      </div>
+
+      <div className={styles.listContent}>
+        {sortedServices.map((service, index) => (
+          <ListViewItem key={index} itemData={service} />
         ))}
-      {laundryServices.length > 0 &&
-        laundryServices.map((service) => <ListViewItem itemData={service} />)}
+      </div>
     </div>
   );
 }
