6a67db9100403ce99b0f78d84e42e95f3ca96880
mrwindupbirdesq <nmaahs11@gmail.com>
2024-05-17 17:31:26 -0700

    Create sort by ratings func

--- src/components/listView/ListView.jsx
+++ src/components/listView/ListView.jsx
@@ -2,6 +2,10 @@ import styles from './list-view.module.css';
 import ListViewItem from '../listViewItem/ListViewItem';
 
 function ListView({ laundryServices, sponsoredServices }) {
+  const sortOptions = (places) => {
+    byRating: () => [...places].sort((a, b) => b.rating - a.rating);
+  };
+
   return (
     <div className={styles.listContainer}>
       {sponsoredServices &&
