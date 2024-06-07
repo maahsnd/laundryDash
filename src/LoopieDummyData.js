71243c168274156c24c0104d889d039ac66e1eec
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-07 11:29:29 -0700

    Link to loopie rather than google

--- src/LoopieDummyData.js
+++ src/LoopieDummyData.js
@@ -32,7 +32,7 @@ const sponsoredServicesByZip = {
 
 for (const zip in loopieServicesByZip) {
   const loopieService = createService(
-    'https://www.google.com/maps/place/Loopie+Laundry/@36.9717026,-121.0665008,4z/data=!3m1!4b1!4m6!3m5!1s0x549015908aee4fd1:0x5a9c65c57b4fbe89!8m2!3d38.9169694!4d-99.8730784!16s%2Fg%2F11g10mt9x6?entry=ttu',
+    'https://loopielaundry.com',
     'https://loopielaundry.app/dashboard',
     'Picks up at your address!',
     '5.0',
