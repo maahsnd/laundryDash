407c8f4bd51f9ef340ae717b986f31bc9c0777d0
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-07 11:25:54 -0700

    Rename loopie data for clarity

--- src/LoopieDummyData.js
+++ src/LoopieDummyData.js
@@ -17,7 +17,7 @@ function createService(
   };
 }
 
-const servicesByZip = {
+const loopieServicesByZip = {
   98144: [],
   98108: [],
   98122: [],
@@ -30,7 +30,7 @@ const sponsoredServicesByZip = {
   98164: ['850 Rainier Ave S, Seattle']
 };
 
-for (const zip in servicesByZip) {
+for (const zip in loopieServicesByZip) {
   const loopieService = createService(
     'https://www.google.com/maps/place/Loopie+Laundry/@36.9717026,-121.0665008,4z/data=!3m1!4b1!4m6!3m5!1s0x549015908aee4fd1:0x5a9c65c57b4fbe89!8m2!3d38.9169694!4d-99.8730784!16s%2Fg%2F11g10mt9x6?entry=ttu',
     'https://loopielaundry.app/dashboard',
@@ -39,9 +39,9 @@ for (const zip in servicesByZip) {
     '25',
     'Loopie Wash and Fold'
   );
-  servicesByZip[zip].push(loopieService);
+  loopieServicesByZip[zip].push(loopieService);
 }
 
-const data = { loopie: servicesByZip, sponsored: sponsoredServicesByZip };
+const data = { loopie: loopieServicesByZip, sponsored: sponsoredServicesByZip };
 
 export default data;
