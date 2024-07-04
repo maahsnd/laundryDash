bf356a53534c46f98ccf5ee0dee431e61e92407f
Nick Maahs <nmaahs11@gmail.com>
2024-07-03 18:19:23 -0700

    Format summary return

--- src/components/chatbot/ChatBot.jsx
+++ src/components/chatbot/ChatBot.jsx
@@ -8,12 +8,9 @@ const Chatbot = ({ places }) => {
     summarizePlaces(places).then(setSummary).catch(console.error);
   }, [places]);
 
-  console.log(places);
-
   const summarizePlaces = async (places) => {
     if (!places || places.length === 0) return "No places found.";
 
-    // Generate a description of the places to send to the AI
     const placesDescription = places
       .map(
         (place) =>
@@ -29,10 +26,9 @@ const Chatbot = ({ places }) => {
 
     const prompt = `Summarize these places by selecting three: 1. the best overall option based on rating and proximity,
     2. the closest option, and 3. the best rated option.  The best overall option must be open now. Answer in this format:
-    '
-    1. The best overall option (based on rating, proximity, and hours) is ... which is ... miles away, has a rating of..., and is open...  hours .
-    2. The closest option is ... which is ... miles away, has a rating of..., and is open...  hours .
-    3. The best rated option is ...which is ... miles away, has a rating of..., and is open...  hours  '
+    The best overall option is ... which is ... miles away, has a rating of..., and is open...  hours .
+    || The closest option is ... which is ... miles away, has a rating of..., and is open...  hours .
+    || The best rated option is ...which is ... miles away, has a rating of..., and is open...  hours.
      ${placesDescription}.`;
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
@@ -57,13 +53,21 @@ const Chatbot = ({ places }) => {
     });
 
     const data = await response.json();
-    console.log("data:  ", data.choices[0].message.content);
-    return data.choices[0].message.content;
+    const split = data.choices[0].message.content.split("||");
+    return split;
   };
 
   return (
     <div>
-      <p>{summary}</p>
+      <h4>AI Summary:</h4>
+      {summary.length > 0 && (
+        <ul>
+          {" "}
+          {summary.map((statement, index) => {
+            return <li key={index}>{statement}</li>;
+          })}
+        </ul>
+      )}
     </div>
   );
 };
